45eb5ea327f943526e41c267e55225d6f887dd80
waffles-code <2089198@students.wits.ac.za>
2022-10-05 21:50:32 +0200

    update model

--- starter-code/dqn/model.py
+++ starter-code/dqn/model.py
@@ -24,17 +24,40 @@ class DQN(nn.Module):
         assert (
             type(action_space) == spaces.Discrete
         ), "action_space must be of type Discrete"
-
+        
+        
+        #  input image is 84 x 84 X 4 
+        #  first hidden layer convolves 16 8 x 8 filters with stride 4
+        #  it means we do a 8 x 8 convolution without padding and stride=4
+        #  stride shrinks the size to ceil((n+f-1)/s) where ’n’ is input dimensions ‘f’ is filter size and ‘s’ is stride length
+        #  the input image will be strinked to ((84+8-1)/4)
+        #  output layer is fully connected with single output for each valid action
+        
+        # num out features in current layer is equal to num in features in lext layer
+  
         self.convolutions = nn.Sequential(
-            nn.Conv2d(observation_space.shape[0], 16, 8, stride=4),
+            
+            #  first hidden layer convolves 16 8 x 8 filters with stride 4
+            nn.Conv2d(observation_space.shape[0], 32, 8, stride=4),
             nn.ReLU(),
-            nn.Conv2d(16, 32, 4, stride=2),
+            #  second layer convolves 32 4x4 filters with stride 2 -> num in_features is 32
+            #  because num out_features in current layer has to be equal to num in_features in lext layer
+            #  i then said num out_fearures for the first layer is 32, idk
+            #  So for the num out_features i said because Deep Q-Learning uses 2 neural networks 
+            #  so at every step weights from the main network are copied to the target network
+            #  so at each step 2 copies of the input are produced 
+            #  so the output will be twice the input
+            #  idk if this understanding is correct
+            nn.Conv2d(32, 64, 4, stride=2),
             nn.ReLU(),
-            nn.Conv2d(32, 32, 3, stride=1),
+            #  final hidden layer is fully connected with 256 rectifier units
+            nn.Conv2d(64, 64, 3, stride=1),
             nn.ReLU(),
         )
 
         self.fully_connected_layer = nn.Sequential(
+            # 49 games were played -> 7x7
+            # 
             nn.Linear(in_features=32*7*7 , out_features=256),
             nn.ReLU(),
             nn.Linear(in_features=256, out_features=action_space.n)
