9ad10050faf2fc45f30b0fee2b18ed1175142815
waffles-code <2089198@students.wits.ac.za>
2022-10-06 22:30:30 +0200

    mybe cos collab timeout looks sus, ill do it @labs

--- plot/make_plot.py
+++ plot/make_plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 rewards = pd.read_csv("./rewards.csv") 
 rewards = rewards.to_numpy()
-plt.plot([i+1 for i in range(0, 105)], rewards[::2])
+plt.plot([i+1 for i in range(0, 145)], rewards[::2])
 plt.xlabel('Episode no.')
 plt.ylabel('Time spent to reach the flag')
 plt.show()
