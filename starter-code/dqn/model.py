7a3d942856d08920f1805801ddfd1b90d8cbe9dd
Fezile Mahlangu <mahlangufezile.3@gmail.com>
2022-10-09 17:43:30 +0200

    new model

--- starter-code/dqn/model.py
+++ starter-code/dqn/model.py
@@ -1,5 +1,7 @@
 from gym import spaces
 import torch.nn as nn
+import torch.nn.functional as F
+
 
 
 class DQN(nn.Module):
@@ -26,42 +28,22 @@ class DQN(nn.Module):
         ), "action_space must be of type Discrete"
         
         
-        #  input image is 84 x 84 X 4 
-        #  first hidden layer convolves 16 8 x 8 filters with stride 4
-        #  it means we do a 8 x 8 convolution without padding and stride=4
-        #  stride shrinks the size to ceil((n+f-1)/s) where ’n’ is input dimensions ‘f’ is filter size and ‘s’ is stride length
-        #  the input image will be strinked to ((84+8-1)/4)
-        #  output layer is fully connected with single output for each valid action
-        
-        # num out features in current layer is equal to num in features in lext layer
-  
-        self.convolutions = nn.Sequential(
-            
-            #  first hidden layer convolves 16 8 x 8 filters with stride 4
-            nn.Conv2d(observation_space.shape[0], 32, 8, stride=4),
-            nn.ReLU(),
-            #  second layer convolves 32 4x4 filters with stride 2 -> num in_features is 32
-            #  because num out_features in current layer has to be equal to num in_features in lext layer
-            #  i then said num out_fearures for the first layer is 32, idk
-            #  So for the num out_features i said because Deep Q-Learning uses 2 neural networks 
-            #  so at every step weights from the main network are copied to the target network
-            #  so at each step 2 copies of the input are produced 
-            #  so the output will be twice the input
-            #  idk if this understanding is correct
-            nn.Conv2d(32, 64, 4, stride=2),
-            nn.ReLU(),
-            #  final hidden layer is fully connected with 256 rectifier units -> mnih paper has 512 
-            nn.Conv2d(64, 64, 3, stride=1),
-            nn.ReLU(),
-        )
+       
+        self.conv1 = nn.Conv2d(observation_space.shape[0],32, 8,stride=4)
 
-        self.fully_connected_layer = nn.Sequential(
-            nn.Linear(in_features=64*7*7 , out_features=512), #512 not 256 
-            nn.ReLU(),
-            nn.Linear(in_features=512, out_features=action_space.n)
-        )
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4,stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3,stride=1)
+      
+        self.fc1 = nn.Linear(in_features=64*7*7, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=action_space.n)
 
     def forward(self, x):
-        output = self.convolutions(x)
-        output = self.fully_connected_layer(output.view(x.size()[0],-1))
-        return output
+      
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, 64 * 7 * 7)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
